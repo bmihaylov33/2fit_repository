3ca79619955a149dc9087a9607eb2c0e26861fe9
Borislav Mihaylov <bmihaylov99@gmail.com>
2025-05-22 11:16:58 +0300

    Modern Fitness Web Application Setup

---
+++ src/components/Exercises/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Exercises() {
+  return (
+    <div className="p-6">
+      <h1 className="text-2xl font-bold mb-4">Exercises</h1>
+      <div className="bg-white rounded-lg shadow p-6">
+        <p>Exercises content will go here</p>
+      </div>
+    </div>
+  );
+}
+
+export default Exercises;
