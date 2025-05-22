1a409b449eac9f8a926f43dde1804e97d816be4f
Borislav Mihaylov <bmihaylov99@gmail.com>
2025-05-22 11:17:15 +0300

    Create missing Exercises component

---
+++ src/components/Profile/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Profile() {
+  return (
+    <div className="p-6">
+      <h1 className="text-2xl font-bold mb-4">Profile</h1>
+      <div className="bg-white rounded-lg shadow p-6">
+        <p>Profile content goes here</p>
+      </div>
+    </div>
+  );
+}
+
+export default Profile;
